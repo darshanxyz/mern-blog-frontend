ca265c7874aea3af8876cf62f173614ae9c5f896
darshanxyz <darshanxyz@gmail.com>
2020-09-16 14:05:22 +0530

    changed the way the cards look on the home page, added a delete button in the post page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class Card extends Component {
 
@@ -7,24 +6,8 @@ class Card extends Component {
     id: '',
   }
 
-  deletePost = event => {
-    event.preventDefault();
-    const post = {
-      id: this.state.id
-    }
-    console.log(post);
-    axios.delete(`http://localhost:4000/${this.state.id}`, post)
-      .then(res => {
-        window.location = "/"
-      })
-  }
-
   readPost = event => {
     window.location = `/${this.props.post._id}`;
-  }
-
-  editPost = event => {
-    window.location = `/${this.props.post._id}/edit`;
   }
 
   componentDidMount() {
@@ -39,15 +22,15 @@ class Card extends Component {
       <div className="post-card">
         <div className="card-body" style={{ backgroundImage: `url("${this.state.imageLink}")` }}>
           {/* <p>{this.props.post.description.length > 300 ? this.props.post.description.slice(0, 300) + ' ...' : this.props.post.description}</p> */}
-        </div>
-        <div className="card-header">
-          <h3 className="post-title">{this.props.post.title}</h3>
-          <h5 className="post-category">{this.props.post.category}</h5>
-        </div>
-        <div className="card-footer">
-          <button onClick={this.deletePost}>Delete</button>
-          <button onClick={this.editPost}>Edit</button>
-          <button style={{ backgroundColor: '#4190c8', color: '#FFF' }} onClick={this.readPost}>Read</button>
+          <div className="overlay" onClick={this.readPost}>
+            <div className="card-header">
+              <h5 className="post-category">{this.props.post.category}</h5>
+              <h3 className="post-title">{this.props.post.title}</h3>
+            </div>
+            <div className="card-footer">
+              <h5>{this.props.post.author}</h5>
+            </div>
+          </div>
         </div>
       </div>
     );
