687ab6ae76723e69685de856bd00b3ba4402e230
darshanxyz <darshanxyz@gmail.com>
2020-09-19 17:30:00 +0530

    added the search bar without actual functionality

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Posts from './Posts';
+import { FaSearch } from 'react-icons/fa';
 
 class Content extends Component {
   render() {
     return (
-      <div className="blog-content">
-        <Posts posts={this.props.posts} />
+      <div className="main-content">
+        <form className="search-form" action="/managePosts">
+          <div className="input-container">
+            <input type="text" placeholder="Search Posts" name="search" />
+            <button className="search-btn" type="submit"><FaSearch color="#FF596C" /></button>
+          </div>
+        </form>
+        <div className="blog-content">
+          <Posts posts={this.props.posts} />
+        </div>
       </div>
     );
   }
