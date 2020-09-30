673ef08e54bfa0ec18b709e4db9b773abc854550
darshanxyz <darshanxyz@gmail.com>
2020-09-30 22:59:58 +0530

    added the search functionality

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import Posts from './Posts';
 import { FaSearch } from 'react-icons/fa';
+import axios from 'axios';
 
 class Content extends Component {
+
+  state = {
+    posts: this.props.posts
+  }
+
+  loadPosts = (event) => {
+    event.preventDefault();
+    const value = event.target.value;
+    if (value && value.length > 5) {
+      axios.get(`http://localhost:4000/search/${value}`)
+        .then(res => {
+          const posts = res.data;
+          this.setState({ posts });
+          console.log(posts);
+        });
+    }
+    if (!value) {
+      axios.get(`http://localhost:4000/`)
+        .then(res => {
+          const posts = res.data;
+          this.setState({ posts });
+          console.log(posts);
+        });
+    }
+  }
+
   render() {
     return (
       <div className="main-content">
-        <form className="search-form" action="/managePosts">
+        <form className="search-form">
           <div className="input-container">
-            <input type="text" placeholder="Search Posts" name="search" />
-            <button className="search-btn" type="submit"><FaSearch color="#FF596C" /></button>
+            <input type="text" placeholder="Search Posts" name="search" onChange={this.loadPosts} />
+            <button className="search-btn" type="submit" onClick={this.loadPosts}><FaSearch color="#FF596C" /></button>
           </div>
         </form>
         <div className="blog-content">
-          <Posts posts={this.props.posts} />
+          <Posts posts={this.state.posts} />
         </div>
       </div>
     );
